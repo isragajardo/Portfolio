9b10a9ceb87862c368dc8dd7c7ce791d4260d665
Isra <igajardojimenez@gmail.com>
2025-01-27 15:29:43 -0300

    making editing changes

--- src/pages/About.js
+++ src/pages/About.js
@@ -24,11 +24,11 @@ function About() {
                 <div className="text-about-me" data-aos="fade-left" data-aos-delay="200">
                     <h1 className="tittle-about">Israel Gajardo</h1>
                     <p>
-                        Hola!   
-                        Sobre mi, soy una persona analítica, ágil para resolver problemas, con mentalidad de
-                        aprendizaje continuo y fácil adaptación a cambios y tecnologías. Además,
-                        tengo habilidad para trabajar en equipo y gestionar tareas de manera
-                        autónoma.
+                    Soy una persona analítica, creativa y con facilidad para resolver problemas. Tengo una mentalidad de aprendizaje continuo, me adapto rápidamente a cambios y tecnologías, y combino mis habilidades técnicas con una excelente capacidad para trabajar en equipo y gestionar tareas de manera autónoma.
+
+Cuento con experiencia en desarrollo web (HTML, CSS, JavaScript, React), administración de CRMs como Zendesk, Hubspot y Bitrix, gestión de ERPs como Oracle Netsuite y Softland, así como en la optimización de procesos y resolución de incidencias. También tengo conocimientos en plataformas como WordPress, Shopify y JumpSeller, gestionando tiendas online, integrando pasarelas de pago y personalizando experiencias de usuario.
+
+Además de mis competencias profesionales, disfruto de actividades como el fútbol, viajar y acampar, las cuales me mantienen activo y motivado para enfrentar nuevos desafíos.
                     </p>
                     <p>
                         En mis tiempos libres me gusta hacer deporte, juego mucho futbol, tambien viajar y acampar
